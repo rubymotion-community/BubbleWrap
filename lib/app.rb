c3e5d02ca8650626bbdd058c883922168c859867
Matt Aimonetti <mattaimonetti@gmail.com>
2012-05-05 14:47:24 +0200

    added the App module to store global states and to offer a persistence layer

---
+++ lib/app.rb
@@ -0,0 +1,40 @@
+# Provides a module to store global states and a persistence layer.
+#
+module App
+  module_function
+
+  @states = {}
+
+  def states
+    @states
+  end
+
+  def name
+    NSBundle.mainBundle.bundleIdentifier
+  end
+
+  # Persistence module built on top of NSUserDefaults
+  module Persistence
+    def self.app_key
+      @app_key ||= App.name
+    end
+
+    def self.[]=(key, value)
+      defaults = NSUserDefaults.standardUserDefaults
+      defaults.setObject(value, forKey: storage_key(key))
+      defaults.synchronize
+    end
+
+    def self.[](key)
+      defaults = NSUserDefaults.standardUserDefaults
+      defaults.objectForKey storage_key(key)
+    end
+
+    private
+
+    def storage_key(key)
+      app_key + '_' + key
+    end
+  end
+
+end
