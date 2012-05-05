2166c1bcdbc856cc25114fcdb1bc26a67e55bd2a
Matt Aimonetti <mattaimonetti@gmail.com>
2012-05-05 14:50:05 +0200

    added a wrapper for ns_index_path

---
+++ lib/ns_index_path.rb
@@ -0,0 +1,23 @@
+class NSIndexPath
+
+  # Gives access to an index at a given position.
+  # @param [Integer] position to use to fetch the index
+  # @return [Integer] the index for the given position
+  def [](position)
+    raise ArgumentError unless position.is_a?(Integer)
+    indexAtPosition(position)
+  end
+
+  # Provides an iterator taking a block following the common Ruby idiom.
+  # @param [Block]
+  # @return [NSIndexPath] the iterated object itself
+  def each
+    i = 0
+    until i == self.length
+      yield self.indexAtPosition(i)
+      i += 1
+    end
+    self
+  end
+
+end
