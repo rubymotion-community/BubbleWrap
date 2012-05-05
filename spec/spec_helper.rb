e759c9fbf7ebba9faef02156be3654a48a0f66b1
Matt Aimonetti <mattaimonetti@gmail.com>
2012-05-05 14:25:00 +0200

    adding the RubyMotion patch required to properly run the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 class TestSuiteDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = UIViewController.alloc.init
     true
   end
 end
