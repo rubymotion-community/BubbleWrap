9a7f2fc49edb5c8f23fa22038d360ea93252e607
Francis Chong <francis@ignition.hk>
2012-05-22 18:54:16 +0800

    raise an error when user try to use this gem in a non rubymotion environment

--- lib/bubble-wrap.rb
+++ lib/bubble-wrap.rb
@@ -1,5 +1,9 @@
 require "bubble-wrap/version"
 
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
+
 Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'bubble-wrap/*.rb')).each do |file|
     app.files.unshift(file)
--- lib/bubble-wrap/version.rb
+++ lib/bubble-wrap/version.rb
@@ -1,3 +1,3 @@
 module BubbleWrap
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
