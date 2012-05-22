5dbb0805432f2bc6b16dacedf650507f06a76cd7
Francis Chong <francis@ignition.hk>
2012-05-22 18:36:11 +0800

    properly include paths

--- lib/bubble-wrap.rb
+++ lib/bubble-wrap.rb
@@ -1,5 +1,7 @@
 require "bubble-wrap/version"
 
 Motion::Project::App.setup do |app|
-  app.files.unshift(Dir.glob(File.join(File.dirname(__FILE__), 'bubble-wrap/*.rb')))
+  Dir.glob(File.join(File.dirname(__FILE__), 'bubble-wrap/*.rb')).each do |file|
+    app.files.unshift(file)
+  end
 end
