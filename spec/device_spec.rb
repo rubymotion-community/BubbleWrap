3712859acdd290dfb316a31dde02ce62fefa5d77
James Harton <jamesotron@gmail.com>
2012-06-01 11:06:41 +1200

    Refactor screen-related methods from Device into Device::Screen

--- spec/device_spec.rb
+++ spec/device_spec.rb
@@ -36,52 +36,6 @@ describe BubbleWrap::Device do
     end
   end
 
-  describe 'on retina enabled screen' do
-    before do
-      @screen = Object.new.tap do |o|
-        def o.respondsToSelector(selector)
-          return true if selector == 'displayLinkWithTarget:selector:'
-          UIDevice.mainDevice.respondsToSelector(selector)
-        end
-        def o.scale
-          2.0
-        end
-        def o.method_missing(*args)
-          UIDevice.mainDevice.send(*args)
-        end
-      end
-    end
-
-    describe '.retina?' do
-      it 'returns true' do
-        BW::Device.retina?(@screen).should == true
-      end
-    end
-  end
-
-  describe 'on non-retina enabled screen' do
-    before do
-      @screen = Object.new.tap do |o|
-        def o.respondsToSelector(selector)
-          return false if selector == 'displayLinkWithTarget:selector:'
-          UIDevice.mainDevice.respondsToSelector(selector)
-        end
-        def o.scale
-          1.0
-        end
-        def o.method_missing(*args)
-          UIDevice.mainDevice.send(*args)
-        end
-      end
-    end
-
-    describe '.retina?' do
-      it 'returns false' do
-        BW::Device.retina?(@screen).should == false
-      end
-    end
-  end
-
   describe 'on device with only front facing camera' do
     before do 
       @picker = Object.new.tap do |o|
@@ -132,60 +86,27 @@ describe BubbleWrap::Device do
     end
   end
 
-  describe '.orientation' do
-
-    describe 'portrait' do
-      it 'returns :portrait' do
-        BW::Device.orientation(UIDeviceOrientationPortrait).should == :portrait
-      end
-    end
-
-    describe 'portrait upside down' do
-      it 'returns :portrait_upside_down' do
-        BW::Device.orientation(UIDeviceOrientationPortraitUpsideDown).should == :portrait_upside_down
-      end
-    end
-
-    describe 'landscape left' do
-      it 'returns :landscape_left' do
-        BW::Device.orientation(UIDeviceOrientationLandscapeLeft).should == :landscape_left
-      end
-    end
-
-    describe 'landscape right' do
-      it 'returns :landscape_right' do
-        BW::Device.orientation(UIDeviceOrientationLandscapeRight).should == :landscape_right
-      end
-    end
-
-    describe 'face up' do
-      it 'returns :face_up' do
-        BW::Device.orientation(UIDeviceOrientationFaceUp).should == :face_up
-      end
-    end
-
-    describe 'face down' do
-      it 'returns :face_down' do
-        BW::Device.orientation(UIDeviceOrientationFaceDown).should == :face_down
-      end
-    end
-
-    describe 'unknown' do
-      it 'returns :unknown' do
-        BW::Device.orientation(UIDeviceOrientationUnknown).should == :unknown
-      end
-    end
-
-    describe 'any other input' do
-      it 'returns :unknown' do
-        BW::Device.orientation('twiggy twiggy twiggy').should == :unknown
-      end
-    end
-  end
-
   describe '.simulator?' do
     it 'returns true' do
       BW::Device.simulator?.should == true
+    end
+  end
+
+  describe '.screen' do
+    it 'return BubbleWrap::Screen' do
+      BW::Device.screen.should == BW::Device::Screen
+    end
+  end
+
+  describe '.retina?' do
+    it 'delegates to BubbleWrap::Screen.retina?' do
+      BW::Device.retina?.should == BW::Device::Screen.retina?
+    end
+  end
+
+  describe '.orientation' do
+    it 'delegates to BubbleWrap::Screen.orientation' do
+      BW::Device.orientation.should == BW::Device::Screen.orientation
     end
   end
 end
