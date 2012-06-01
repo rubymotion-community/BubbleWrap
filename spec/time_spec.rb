725da17fe6958e0cc47d2edabcf263bb5851b27d
James Miller <bensie@gmail.com>
2012-06-01 14:55:52 -0700

    One assertion per 'it'

--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -1,13 +1,33 @@
 describe "Time" do
 
-  it "should parse an iso8601 formatted time to a Time object" do
-    time = Time.iso8601("2012-05-31T19:41:33Z")
-    time.year.should  == 2012
-    time.month.should == 5
-    time.day.should   == 31
-    time.hour.should  == 19
-    time.min.should   == 41
-    time.sec.should   == 33
+  describe "parsing an iso8601 formatted time to a Time object" do
+    before do
+      @time = Time.iso8601("2012-05-31T19:41:33Z")
+    end
+
+    it "should have a valid year" do
+      @time.year.should == 2012
+    end
+
+    it "should have a valid month" do
+      @time.month.should == 5
+    end
+
+    it "should have a valid day" do
+      @time.day.should == 31
+    end
+
+    it "should have a valid hour" do
+      @time.hour.should == 19
+    end
+
+    it "should have a valid minute" do
+      @time.min.should == 41
+    end
+
+    it "should have a valid second" do
+      @time.sec.should == 33
+    end
   end
 
 end
