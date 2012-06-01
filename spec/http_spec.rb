3df18892b84bb5f8e33b15593c4f2100fde5bb6a
Marin Usalj <mneorr@gmail.com>
2012-06-01 20:47:49 +0200

    Small cleanup

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -49,56 +49,70 @@ describe "HTTP::Query" do
     @query.should.respond_to :options
   end
 
-  it "should set default timeout to 30s or the one from hash" do
-    @query.instance_variable_get(:@timeout).should == 30
-    
-    new_query = BubbleWrap::HTTP::Query.new( 'http://localhost/', :get, {timeout: 10})
-    new_query.instance_variable_get(:@timeout).should == 10
+  describe "When initialized" do
+
+    it "should set default timeout to 30s or the one from hash" do
+      @query.instance_variable_get(:@timeout).should == 30
+
+      new_query = BubbleWrap::HTTP::Query.new( 'http://localhost/', :get, {timeout: 10})
+      new_query.instance_variable_get(:@timeout).should == 10
+    end
+
+    it "should create params with nested hashes with prefix[key]=value" do
+      payload = { 
+        user: { name: 'marin', surname: 'usalj' }, 
+        twitter: '@mneorr',
+        website: 'mneorr.com',
+        values: [1, 2, 3]
+      }
+      expected_params = [
+        'user[name]=marin', 
+        'user[surname]=usalj', 
+        'twitter=@mneorr',
+        'website=mneorr.com',
+        'values=[1, 2, 3]'
+      ]
+      @query.generate_get_params(payload).should.equal expected_params
+    end
+
+    it "should assign status_code, headers and response_size on didReceiveResponse:" do
+      headers = { foo: 'bar' }
+      status_code = 234
+      length = 123.53
+
+      response = FakeURLResponse.new(status_code, headers, length)
+      @query.connection(nil, didReceiveResponse:response)
+
+      @query.status_code.should.equal status_code
+      @query.response_headers.should.equal headers
+      @query.response_size.should.equal length
+    end
+
   end
 
-  it "should create params with nested hashes with prefix[key]=value" do
-    payload = { 
-      user: { name: 'marin', surname: 'usalj' }, 
-      twitter: '@mneorr',
-      website: 'mneorr.com',
-      values: [1, 2, 3]
-    }
-    expected_params = [
-      'user[name]=marin', 
-      'user[surname]=usalj', 
-      'twitter=@mneorr',
-      'website=mneorr.com',
-      'values=[1, 2, 3]'
-    ]
-    @query.generate_get_params(payload).should.equal expected_params
+  describe "when didRecieveData:" do
+
+    def query_received_data
+      @query.instance_variable_get(:@received_data)
+    end
+
+    it "should initialize @received_data and append the received data" do
+      query_received_data.should.equal nil
+      data = NSData.dataWithBytesNoCopy(Pointer.new(:char, 'abc'), length:24)
+
+      @query.connection(nil, didReceiveData:nil)
+      query_received_data.should.not.equal nil
+
+      @query.connection(nil, didReceiveData:data)
+      query_received_data.length.should.equal 24
+
+      @query.connection(nil, didReceiveData:data)
+      query_received_data.length.should.equal 48
+    end  
+
   end
 
-  it "should assign status_code, headers and response_size on didReceiveResponse:" do
-    headers = { foo: 'bar' }
-    status_code = 234
-    length = 123.53
 
-    response = FakeURLResponse.new(status_code, headers, length)
-    @query.connection(nil, didReceiveResponse:response)
-
-    @query.status_code.should.equal status_code
-    @query.response_headers.should.equal headers
-    @query.response_size.should.equal length
-  end
-
-  it "should initialize iVar and append the received data on didReceiveData:" do
-    query_received_data.should.equal nil
-    data = NSData.dataWithBytesNoCopy(Pointer.new(:char, 'abc'), length:24)
-    
-    @query.connection(nil, didReceiveData:nil)
-    query_received_data.should.not.equal nil
-    
-    @query.connection(nil, didReceiveData:data)
-    query_received_data.length.should.equal 24
-
-    @query.connection(nil, didReceiveData:data)
-    query_received_data.length.should.equal 48
-  end
 
   describe "when requestDidFailWithError:" do
     before do
@@ -107,21 +121,21 @@ describe "HTTP::Query" do
 
     it "should turn off network indicator" do
       UIApplication.sharedApplication.isNetworkActivityIndicatorVisible.should == true
-      
+
       @query.connection(nil, didFailWithError:@fake_error)
       UIApplication.sharedApplication.isNetworkActivityIndicatorVisible.should == false    
     end
 
     it "should set request_done to true" do
       @query.request.done_loading.should == false
-      
+
       @query.connection(nil, didFailWithError:@fake_error)
       @query.request.done_loading.should == true
     end
 
     it "should set the error message to response object" do
       @query.response.error_message.should.equal nil
-      
+
       @query.connection(nil, didFailWithError:@fake_error)
       @query.response.error_message.should.equal @fake_error.localizedDescription
     end
@@ -130,7 +144,7 @@ describe "HTTP::Query" do
       expected_response = BubbleWrap::HTTP::Response.new
       real_response = nil
       block = lambda{ |response, query| real_response = response }
-      
+
       query = BubbleWrap::HTTP::Query.new( 'http://localhost', :get, { :action => block })
       query.instance_variable_set(:@response, expected_response)
 
@@ -144,14 +158,14 @@ describe "HTTP::Query" do
 
     it "should turn off the network indicator" do
       UIApplication.sharedApplication.isNetworkActivityIndicatorVisible.should == true
-      
+
       @query.connectionDidFinishLoading(nil)
       UIApplication.sharedApplication.isNetworkActivityIndicatorVisible.should == false
     end
 
     it "should set request_done to true" do
       @query.request.done_loading.should == false
-      
+
       @query.connectionDidFinishLoading(nil)
       @query.request.done_loading.should == true
     end
@@ -161,7 +175,7 @@ describe "HTTP::Query" do
       headers = { foo: 'bar' }
       status_code = 234
       response = FakeURLResponse.new(status_code, headers, 65456)
-      
+
       @query.connection(nil, didReceiveResponse:response)
       @query.connection(nil, didReceiveData:data)
       @query.connectionDidFinishLoading(nil)
@@ -176,7 +190,7 @@ describe "HTTP::Query" do
       expected_response = BubbleWrap::HTTP::Response.new
       real_response = nil
       block = lambda{ |response, query| real_response = response }
-      
+
       query = BubbleWrap::HTTP::Query.new( 'http://localhost', :get, { :action => block })
       query.instance_variable_set(:@response, expected_response)
 
@@ -196,7 +210,7 @@ describe "HTTP::Query" do
       @query.instance_variable_set(:@headers, expected_headers)
 
       new_request = @query.connection(nil, willSendRequest:@request, redirectResponse:nil)
-      
+
       @query.request.should.not.be.equal @request
       new_request.URL.description.should.equal @request.URL.description
       new_request.allHTTPHeaderFields.should.equal expected_headers
@@ -218,10 +232,6 @@ describe "HTTP::Query" do
       @query.connection(nil, willSendRequest:@request, redirectResponse:nil)
       @query.connection.request.should.equal @request
     end
-  end
-
-  def query_received_data
-    @query.instance_variable_get(:@received_data)
   end
 
   class BubbleWrap::HTTP::Query
