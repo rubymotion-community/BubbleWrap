70331a048a741a63b383bc57fb7685b589b4fb4f
Marin Usalj <mneorr@gmail.com>
2012-06-08 11:10:34 +0200

    Improved HTTP specs

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -7,13 +7,18 @@ describe "HTTP" do
   describe "Core HTTP method calls" do
 
     def test_http_method(method)
-      delegator = Proc.new { |q| @the_query = q }
+      called = false
+      delegator = Proc.new { |r, q| @the_response = r; @the_query = q; called = true }
       query = BubbleWrap::HTTP.send(method, @localhost_url, { name: 'bubble-wrap', action: delegator })
       query.should.not.equal nil
       query.method.should.equal method.to_s.upcase
       query.options[:name].should.equal 'bubble-wrap'
       query.instance_variable_get(:@delegator).should.equal delegator
-      @query.should.be.same_as @the_query
+
+      query.connectionDidFinishLoading(query.connection)
+      query.should.be.same_as @the_query
+      query.response.should.be.same_as @the_response
+      called.should.equal true
     end
 
     it ".get .post .put .delete .head .patch should properly generate the HTTP::Query" do
@@ -23,12 +28,15 @@ describe "HTTP" do
     end
 
     it "uses the block instead of action passed in " do
-      expected_delegator = Proc.new {|response|}
-      
       [:get, :post, :put, :delete, :head, :patch].each do |method|  
+        called = false
+        expected_delegator = Proc.new {|response| called = true }
+
         query = BubbleWrap::HTTP.send(method, @localhost_url, { action: 'not_valid' }, &expected_delegator)
         query.instance_variable_get(:@delegator).should.equal expected_delegator
-      end
+        query.connectionDidFinishLoading(query.connection)
+        called.should.equal true
+      end 
     end
 
   end
