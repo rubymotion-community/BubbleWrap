d612555a98113a48e861d3aaeebfc03c8591c3ba
James Harton <jamesotron@gmail.com>
2012-06-13 14:16:12 +1200

    Add specs for BW::Deferrable::Eventable

--- motion/dispatch/eventable.rb
+++ motion/dispatch/eventable.rb
@@ -1,16 +1,21 @@
 module BubbleWrap
   module Dispatch
+    # A simple mixin that adds events to your object.
     module Eventable
 
+      # When `event` is triggered the block will execute
+      # and be passed the arguments that are passed to
+      # `trigger`.
       def on(event, &blk)
-        @events ||= Hash.new { [] }
-        @events[event].unshift blk
+        @events ||= Hash.new { |h,k| h[k] = [] }
+        @events[event].push blk
       end
 
+      # Trigger an event
       def trigger(event, *args)
-        @events ||= Hash.new { [] }
+        @events ||= Hash.new { |h,k| h[k] = [] }
         @events[event].map do |event|
-          ::BubbleWrap::Dispatch.schedule(*args,&event)
+          event.call(*args)
         end
       end
 
---
+++ spec/motion/dispatch/eventable_spec.rb
@@ -0,0 +1,40 @@
+describe BubbleWrap::Dispatch::Eventable do
+  before do
+    @subject = Class.new do
+      include BubbleWrap::Dispatch::Eventable
+    end.new
+    @proxy = Class.new do 
+      attr_accessor :proof
+    end.new
+  end
+
+  describe '.on' do
+    it 'registers events' do
+      proof = proc {  }
+      @subject.on(:foo, &proof)
+      events = @subject.instance_variable_get(:@events)
+      events[:foo].member?(proof).should == true
+    end
+  end
+
+  describe '.trigger' do
+    it 'calls event procs' do
+      @proxy.proof = false
+      @subject.on(:foo) do |r|
+        @proxy.proof = r
+      end
+      @subject.trigger(:foo, true)
+      @proxy.proof.should == true
+    end
+
+    it 'calls all the event procs' do
+      @proxy.proof = 0
+      @subject.on(:foo) { |r| @proxy.proof += r }
+      @subject.on(:foo) { |r| @proxy.proof += r }
+      @subject.on(:foo) { |r| @proxy.proof += r }
+      @subject.trigger(:foo, 2)
+      @proxy.proof.should == 6
+    end
+  end
+
+end
