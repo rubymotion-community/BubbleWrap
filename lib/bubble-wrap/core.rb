adf3135b59efc5733486132126aee9437c415878
Clay Allsopp <clay.allsopp@gmail.com>
2012-06-26 22:30:48 -0400

    require ui in core for now

--- lib/bubble-wrap/core.rb
+++ lib/bubble-wrap/core.rb
@@ -4,3 +4,4 @@ BubbleWrap.require('motion/core/**/*.rb') do
   file('motion/core/device/screen.rb').depends_on 'motion/core/device.rb'
   file('motion/core/pollute.rb').depends_on 'motion/core/ns_index_path.rb'
 end
+require 'bubble-wrap/ui'
