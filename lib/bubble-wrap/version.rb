6154a520bdeb47daae58445e634b6436f97750a8
Marin Usalj <mneorr@gmail.com>
2013-01-21 22:38:17 -0800

    updated bubble-wrap version

--- lib/bubble-wrap/version.rb
+++ lib/bubble-wrap/version.rb
@@ -1,5 +1,5 @@
 module BubbleWrap
-  VERSION = '1.1.4' unless defined?(BubbleWrap::VERSION)
+  VERSION = '1.1.5' unless defined?(BubbleWrap::VERSION)
   MIN_MOTION_VERSION = '1.24'
 
   module_function
