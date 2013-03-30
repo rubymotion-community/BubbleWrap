127a4e9b7d5959ece81bf367c1322fc041483a4d
Robert Beene <rbeene@teladoc.com>
2013-03-30 23:34:57 +0100

    UIAlertView
    Add module native_attributes to handle aliasing and converting camel-
    case methods to snake_case.

---
+++ motion/wrappers/alert.rb
@@ -0,0 +1,37 @@
+module Wrappers
+  class Alert
+    extend BW::Wrappers::NativeAttributes
+
+    native_attributes :show, :title, :message, :numberOfButtons, :cancelButtonIndex, :delegate, :visible
+    attr_accessor :native
+    aliased_native_attributes style: :alertViewStyle
+
+    STYLE_OPTIONS = %w(default secure plain login)
+    STYLE_DEFAULT = 0
+    STYLE_SECURE  = 1
+    STYLE_PLAIN   = 2
+    STYLE_LOGIN   = 3
+
+    def initialize(title, message, options={})
+      delegate              = options[:delegate]
+      cancel_button         = options[:cancel_button]||"OK"
+      other_buttons         = options[:other_buttons]
+      style                 = options[:style] ? BW::Alert.const_get("STYLE_#{options[:style].upcase}") : STYLE_DEFAULT
+      @native               = UIAlertView.alloc.initWithTitle(title, message: message,
+                                  delegate: delegate, cancelButtonTitle: cancel_button, otherButtonTitles: other_buttons)
+      style                 = style
+      self
+    end
+
+    def style=(style)
+      if BW::Alert.const_defined?("STYLE_#{style.upcase}")
+        @native.alertViewStyle = BW::Alert.const_get("STYLE_#{style.upcase}")
+      else
+        raise "UIView Alert Style #{style} does not exist. Valid options are #{STYLE_OPTIONS.join(", ")}"
+      end
+      @native
+    end
+
+  end
+  ::Alert = BubbleWrap::Wrappers::Alert unless defined?(::Alert)
+end
