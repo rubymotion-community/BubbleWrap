42b60b96ba73111a2ef37435599ba6dd1ffa021c
Clay Allsopp <clay.allsopp@gmail.com>
2013-04-03 20:23:16 -0700

    util/constants dependencies

--- lib/bubble-wrap/location.rb
+++ lib/bubble-wrap/location.rb
@@ -1,7 +1,8 @@
 require 'bubble-wrap/loader'
-BubbleWrap.require('motion/core/constants.rb')
+BubbleWrap.require('motion/util/constants.rb')
 BubbleWrap.require('motion/core/string.rb')
 BubbleWrap.require('motion/location/**/*.rb') do
   file('motion/location/pollute.rb').depends_on 'motion/location/location.rb'
+  file('motion/location/location.rb').depends_on 'motion/util/constants.rb'
   file('motion/location/location.rb').uses_framework('CoreLocation')
 end
