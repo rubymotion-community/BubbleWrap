07b45e3e854716c7bc48b435468a6bb209306118
seanlilmateus <seanlilmateus@yahoo.de>
2013-05-16 21:18:26 +0200

    moved BW::Reactor::PeriodicTimer from NSTimer to GCD Source.timer

--- motion/reactor/periodic_timer.rb
+++ motion/reactor/periodic_timer.rb
@@ -8,7 +8,6 @@ module BubbleWrap
 
       # Create a new timer that fires after a given number of seconds
       def initialize(interval, *args, &blk)
-        options = args.last.is_a?(Hash) ? args.last : {}
         callback = args.first.respond_to?(:call) ? args.first : blk
         raise ArgumentError, "No callback or block supplied to periodic timer" unless callback
 
@@ -17,14 +16,14 @@ module BubbleWrap
           callback.call
           trigger(:fired)
         }
-        @timer = NSTimer.timerWithTimeInterval(interval, target: fire, selector: 'call:', userInfo: nil, repeats: true)
-        runloop_mode = options[:common_modes] ? NSRunLoopCommonModes : NSDefaultRunLoopMode 
-        NSRunLoop.currentRunLoop.addTimer(@timer, forMode: runloop_mode)
+        queue  = Dispatch::Queue.current
+        @timer = Dispatch::Source.timer(interval, interval, 0.0, queue, &fire)
       end
 
       # Cancel the timer
       def cancel
-        @timer.invalidate
+        @timer.cancel!
+        @timer = nil
         trigger(:cancelled)
       end
 
