346743976929f0ceb9bd14ad2424a88bb826d9d0
John Z Wu <bogardon@gmail.com>
2013-06-17 14:03:52 -0700

    [time_spec.rb] added spec for parsing ISO8601 Date string's that contain the full timezone. revived spec for timezones.

--- spec/motion/core/time_spec.rb
+++ spec/motion/core/time_spec.rb
@@ -17,39 +17,49 @@ describe "Time" do
   describe "parsing an iso8601 formatted time to a Time object" do
     before do
       @time = Time.iso8601("2012-05-31T19:41:33Z")
+      @time_with_timezone = Time.iso8601_with_timezone("1987-08-10T06:00:00+02:00")
     end
 
     it "should be a time" do
       @time.instance_of?(Time).should == true
+      @time_with_timezone.instance_of?(Time).should == true
     end
 
-    # # Crashes Buggy RubyMotion 1.18
-    # it "should be converted to the local timezone automatically" do
-    #   @time.zone.should == Time.now.zone
-    # end
+    # Crashes Buggy RubyMotion 1.18
+    it "should be converted to the local timezone automatically" do
+      local_zone = Time.now.zone
+      @time.zone.should == local_zone
+      @time_with_timezone.zone == local_zone
+    end
 
     it "should have a valid year" do
       @time.utc.year.should == 2012
+      @time_with_timezone.utc.year.should == 1987
     end
 
     it "should have a valid month" do
       @time.utc.month.should == 5
+      @time_with_timezone.utc.month.should == 8
     end
 
     it "should have a valid day" do
       @time.utc.day.should == 31
+      @time_with_timezone.utc.day.should == 10
     end
 
     it "should have a valid hour" do
       @time.utc.hour.should == 19
+      @time_with_timezone.utc.hour.should == 4
     end
 
     it "should have a valid minute" do
       @time.utc.min.should == 41
+      @time_with_timezone.utc.min.should == 0
     end
 
     it "should have a valid second" do
       @time.utc.sec.should == 33
+      @time_with_timezone.utc.sec.should == 0
     end
   end
 
