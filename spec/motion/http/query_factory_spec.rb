ce4345ac10348565d295a46198eb265184941fa2
Colin T.A. Gray <colinta@gmail.com>
2014-01-16 11:35:19 -0700

    fix specs to test the 'query' code (connection is lazily created)

--- spec/motion/http/query_factory_spec.rb
+++ spec/motion/http/query_factory_spec.rb
@@ -10,49 +10,49 @@ describe BW::HTTP::QueryFactory do
       query = BW::HTTP.query.get("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `post` request" do
       query = BW::HTTP.query.post("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `put` request" do
       query = BW::HTTP.query.put("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `delete` request" do
       query = BW::HTTP.query.delete("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `head` request" do
       query = BW::HTTP.query.head("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `options` request" do
       query = BW::HTTP.query.options("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
     it "should create `patch` request" do
       query = BW::HTTP.query.patch("http://google.com")
       query.should.is_a?(BW::HTTP::Query)
       query.started?.should.equal false
-      query.connection.was_started.should.equal false
+      query.connection.should.equal nil
     end
 
   end
