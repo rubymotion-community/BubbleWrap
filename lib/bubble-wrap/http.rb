2cfa97d5dffd7a36c620a2487c9ffddd53edc5a4
Clay Allsopp <clay.allsopp@gmail.com>
2014-02-17 12:14:56 -0800

    fix /core and /http to be able to load separately

--- lib/bubble-wrap/http.rb
+++ lib/bubble-wrap/http.rb
@@ -1,3 +1,4 @@
+require 'bubble-wrap/version' unless defined?(BubbleWrap::VERSION)
 require 'bubble-wrap/loader'
 BubbleWrap.require('motion/core/ns_url_request.rb')
 BubbleWrap.require('motion/core.rb')
