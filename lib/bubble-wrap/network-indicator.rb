dc8eacf9dcd3d833bc718a52df13abb4489a89f8
Colin T.A. Gray <colinta@gmail.com>
2014-03-06 10:27:42 -0700

    don't need this block

--- lib/bubble-wrap/network-indicator.rb
+++ lib/bubble-wrap/network-indicator.rb
@@ -2,7 +2,5 @@ require 'bubble-wrap/loader'
 
 BubbleWrap.require_ios("network-indicator") do
   BubbleWrap.require('motion/core/app.rb')
-  BubbleWrap.require('motion/network-indicator/**/*.rb') do
-    # file('motion/network-indicator/network-indicator.rb').depends_on('motion/network-indicator/result.rb')
-  end
+  BubbleWrap.require('motion/network-indicator/**/*.rb')
 end
