dfda4dc23f308c6c6c9aa141d329fdb536d0e034
Clay Allsopp <clay.allsopp@gmail.com>
2014-04-15 16:08:59 -0700

    UI now depends on constants.rb

--- lib/bubble-wrap/ui.rb
+++ lib/bubble-wrap/ui.rb
@@ -1,6 +1,7 @@
 require 'bubble-wrap/loader'
 
 BubbleWrap.require_ios("ui") do
+  BubbleWrap.require('motion/util/constants.rb')
   BubbleWrap.require('motion/ui/**/*.rb') do
     file('motion/ui/pollute.rb').depends_on %w(
       motion/ui/ui_control_wrapper.rb
