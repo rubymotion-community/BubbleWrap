64a6beed1da067f8ebf621b38cfcb2c9050b1f57
Mark Rickert <mjar81@gmail.com>
2014-05-14 13:58:33 -0400

    Really fix uninitialized constants for ios 6 this time.

--- motion/ui/ui_activity_view_controller_wrapper.rb
+++ motion/ui/ui_activity_view_controller_wrapper.rb
@@ -44,11 +44,22 @@ module BW
     UIActivityTypeSaveToCameraRoll
   )
 
-  Constants.register(
-    UIActivityTypeAddToReadingList,
-    UIActivityTypePostToFlickr,
-    UIActivityTypePostToVimeo,
-    UIActivityTypePostToTencentWeibo,
-    UIActivityTypeAirDrop
-  ) if Device.ios_version >= "7.0"
+  # These constants are not iOS 6 compatable.
+  # Define them but don't initialize them unless we're on iOS 7
+  if defined?(UIActivityViewController::UIActivityTypeAddToReadingList).nil?
+    Kernel.const_set('UIActivityTypeAddToReadingList', '')
+    Kernel.const_set('UIActivityTypePostToFlickr', '')
+    Kernel.const_set('UIActivityTypePostToVimeo', '')
+    Kernel.const_set('UIActivityTypePostToTencentWeibo', '')
+  end
+
+  if Device.ios_version >= "7.0"
+    Constants.register(
+      Kernel.const_get('UIActivityTypeAddToReadingList'),
+      Kernel.const_get('UIActivityTypePostToFlickr'),
+      Kernel.const_get('UIActivityTypePostToVimeo'),
+      Kernel.const_get('UIActivityTypePostToTencentWeibo'),
+      Kernel.const_get('UIActivityTypeAirDrop')
+    )
+  end
 end
