6e7c6e99be9d7c90acb512c0dd896b0b3906f6c3
Mark Rickert <mjar81@gmail.com>
2014-06-09 16:26:04 -0400

    I think I got iOS 7 only files working properly.

--- lib/bubble-wrap/ui.rb
+++ lib/bubble-wrap/ui.rb
@@ -2,7 +2,7 @@ require 'bubble-wrap/loader'
 
 BubbleWrap.require_ios("ui") do
   BubbleWrap.require('motion/util/constants.rb')
-  BubbleWrap.require_ios('motion/util/ios_7_constants.rb') if App.config.send(:deployment_target).to_f >= 7.0
+  BubbleWrap.require('motion/ios/7/uiactivity_view_controller_constants.rb') if App.config.send(:deployment_target).to_f >= 7.0
   BubbleWrap.require('motion/ui/**/*.rb') do
     file('motion/ui/pollute.rb').depends_on %w(
       motion/ui/ui_control_wrapper.rb
---
+++ motion/ios/7/uiactivity_view_controller_constants.rb
@@ -0,0 +1,10 @@
+module BW
+  # iOS 7 ONLY Constants for UIActivityViewController
+  Constants.register(
+    UIActivityTypeAddToReadingList,
+    UIActivityTypePostToFlickr,
+    UIActivityTypePostToVimeo,
+    UIActivityTypePostToTencentWeibo,
+    UIActivityTypeAirDrop
+  )
+end
