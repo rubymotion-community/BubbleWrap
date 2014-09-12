67f8d4cec9494c55c9e5560e32d1d808b14777bc
Jon Morehouse <morehousej09@gmail.com>
2014-09-11 18:34:02 -0700

    Added EM::ThreadAwareDeferrable. Fixes #410

--- motion/reactor/thread_aware_deferrable.rb
+++ motion/reactor/thread_aware_deferrable.rb
@@ -6,13 +6,31 @@ module BubbleWrap
       
       # need to store the the queue in callback / errback
       def callback(&blk)
+        return unless blk
+        cache_block_queue(&blk)
         super(&blk)
-        @block_queues ||= {}
-        @block_queues[blk.object_id] = Dispatch::Queue.current
-
-        puts @block_queues
       end
 
+      def errback(&blk)
+        return unless blk
+        cache_block_queue(&blk)
+        super(&blk)
+      end
+
+      def execute_block(&blk)
+        return unless blk
+        queue = @queue_cache.delete(blk.object_id)
+        return unless queue
+        queue.async do
+          blk.call(*@deferred_args)
+        end
+      end
+
+      def cache_block_queue(&blk)
+        return unless blk
+        @queue_cache ||= {}
+        @queue_cache[blk.object_id] = Dispatch::Queue.current
+      end
     end
   end
 end
--- spec/motion/reactor/thread_aware_deferrable_spec.rb
+++ spec/motion/reactor/thread_aware_deferrable_spec.rb
@@ -1,3 +1,50 @@
+shared :queue_caching_deferrable_method do
+  it "stores the current queue that is being used when a callback is added" do
+    @subject.send(@method, &@blk)
+    cache = @subject.instance_variable_get("@queue_cache")
+    cache.should.not.be.nil
+    cache[@blk.object_id].to_s.should.equal Dispatch::Queue.main.to_s
+  end
+
+  it "stores the queue, even if its not the main" do
+    @queue.async do 
+      @subject.send(@method, &@blk)
+      Dispatch::Queue.main.async { resume }
+    end
+    wait do
+      cache = @subject.instance_variable_get("@queue_cache")
+      cache.should.not.be.nil
+      cache[@blk.object_id].to_s.should.equal @queue.to_s
+    end
+  end
+end
+
+shared :queue_block_execution do
+  it "calls the block on the right thread, with deferred argument once the deferrable is finished" do
+    @queue.async do 
+      @subject.send(@block_method) do |*args|
+        Dispatch::Queue.current.to_s.should.equal @queue.to_s
+        args.should.equal [true]
+        Dispatch::Queue.main.async { resume }
+      end
+    end
+    @subject.send(@status_method, true)
+
+    wait {}
+  end
+
+  it "removes the queue from internal cache once the deferrable is finished" do
+    @subject.send(@block_method) do |*args| 
+      Dispatch::Queue.main.async { resume }
+    end
+    @subject.send(@status_method, true)
+
+    wait do
+      @subject.instance_variable_get("@queue_cache").length.should.equal 0
+    end
+  end
+end
+
 describe BubbleWrap::Reactor::ThreadAwareDeferrable do
 
   before do
@@ -6,31 +53,33 @@ describe BubbleWrap::Reactor::ThreadAwareDeferrable do
     @blk = Proc.new {|*args| }
   end
 
-  def resume_on_main
-    Dispatch::Queue.main.async { resume }
-  end
-
   describe '.callback' do
-    it "stores the current queue that is being used when a callback is added" do
-
-      @subject.callback &@blk
-
-      wait_max 3.0 {}
-      true.should.equal true
-
-
+    before do
+      @method = :callback
     end
+    behaves_like :queue_caching_deferrable_method
   end
 
   describe '.errback' do
-
+    before do
+      @method = :errback
+    end
+    behaves_like :queue_caching_deferrable_method
   end
 
   describe '.succeed' do
-
+    before do
+      @block_method = :callback
+      @status_method = :succeed
+    end
+    behaves_like :queue_block_execution
   end
 
   describe '.fail' do
-
+    before do
+      @block_method = :callback
+      @status_method = :succeed
+    end
+    behaves_like :queue_block_execution
   end
 end
