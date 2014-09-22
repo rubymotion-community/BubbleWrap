cc9aa3997821211338590e1dc89dd8fe707d918a
Colin T.A. Gray <colinta@gmail.com>
2014-09-22 11:42:59 -0600

    whitespace

--- lib/bubble-wrap/all.rb
+++ lib/bubble-wrap/all.rb
@@ -4,12 +4,13 @@ require File.expand_path('../loader', __FILE__)
   'font',
   'http',
   'location',
-  'mail','sms',
+  'mail',
   'media',
   'motion',
   'network-indicator',
   'reactor',
   'rss_parser',
+  'sms',
   'ui',
 ].each { |sub|
 	require File.expand_path("../#{sub}", __FILE__)
